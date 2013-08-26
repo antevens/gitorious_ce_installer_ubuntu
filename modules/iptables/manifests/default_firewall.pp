38abbb5cd0f47e093a086b8d5fbb14fc4911166a
Jarl Stefansson <jarl.stefansson@gmail.com>
2013-08-26 16:09:07 -0400

    Fixed iptables scripts to work with ubuntu

--- modules/iptables/manifests/default_firewall.pp
+++ modules/iptables/manifests/default_firewall.pp
@@ -1,15 +1,43 @@
 class iptables::default_firewall {
-  file { "/etc/sysconfig/iptables":
+
+  case $operatingsystem {
+    CentOS, RedHat: { $fw_rules_file = "/etc/sysconfig/iptables"}
+    Ubuntu, Debian: { $fw_rules_file = "/etc/iptables.rules"}
+ }
+
+  file { "${fw_rules_file}":
     ensure => present,
     source => "puppet:///modules/iptables/default_firewall",
     owner => "root",
     group => "root",
     mode => "0600",
-    notify => Service["iptables"],
   }
 
-  service { "iptables":
-    ensure => running,
-    enable => true,
+  case $operatingsystem {
+    Ubuntu, Debian: { 
+      file { "/etc/network/if-pre-up.d/iptablesload":
+        ensure => present,
+        source => "puppet:///modules/iptables/iptablesload",
+        owner => "root",
+        group => "root",
+        mode => "0700",
+      }
+      file { "/etc/network/if-post-down.d/iptablessave":
+        ensure => present,
+        source => "puppet:///modules/iptables/iptablessave",
+        owner => "root",
+        group => "root",
+        mode => "0700",
+      }
+      exec {"restart_networking":
+        command => "nohup sh -c "invoke-rc.d networking stop; sleep 2; invoke-rc.d networking start",
+      }
+    }
+    CentOS, RedHat: {
+      service { "iptables":
+        ensure => running,
+        enable => true,
+      }
+    }
   }
 }
