a98cdef5b282c5f9a45bbee9aa20063edbabaf0b
Jarl Stefansson <jarl.stefansson@gmail.com>
2013-08-26 17:15:38 -0400

    Fixed redis, added dependency in resque module instead of repeating work

--- modules/gitorious/manifests/redis.pp
+++ modules/gitorious/manifests/redis.pp
@@ -1,11 +1,24 @@
 class gitorious::redis {
-  package { "redis":
+
+  case $operatingsystem {
+    "CentOS", "RedHat": { 
+        $package_list = ["redis"]
+        $redis_service_name = "redis"
+    }
+    "Ubuntu", "Debian": {
+        $package_list = ["redis-server"]
+        $redis_service_name = "redis-server"
+    }
+  }
+
+  package { "${package_list}":
     ensure => installed,
   }
 
   service { "redis":
+    name => "${redis_service_name}",
     enable => true,
     ensure => running,
-    require => Package["redis"],
+    require => Package["${package_list}"],
   }
 }
--- modules/resque/manifests/init.pp
+++ modules/resque/manifests/init.pp
@@ -1,15 +1,7 @@
 class resque {
+  require gitorious::redis
+
   $resque_gitorious_root = $gitorious::app_root
-
-  package { "redis":
-    ensure => installed,
-  }
-
-  service { "redis":
-    ensure => running,
-    enable => true,
-    require => Package["redis"],
-  }
 
   file { "/etc/init/resque-worker.conf":
     ensure => present,
