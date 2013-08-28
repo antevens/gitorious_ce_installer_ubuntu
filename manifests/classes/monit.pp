36b9042fe16b23a8d04e7f5e2393fa0fd8245ee4
Jarl Stefansson <jarl@sdelements.com>
2013-08-28 17:38:05 -0400

    Fixing monit configs

--- manifests/classes/monit.pp
+++ manifests/classes/monit.pp
@@ -2,14 +2,12 @@ class monit {
 
   case $operatingsystem {
     "CentOS", "RedHat": { 
-        $monit_confd_dir = ["/etc/monit.d"]
+        $monit_confd_dir = "/etc/monit.d"
     }
     "Ubuntu", "Debian": {
-        $monit_confd_dir = ["/etc/monit/conf.d"]
+        $monit_confd_dir = "/etc/monit/conf.d"
     }
   }
-
-
 
   define config($t_app_root="", $t_control_scripts_dir="", $fqdn=false, $pids_dir="", $pidfile="", $repo_root="") {
     file{"${monit_confd_dir}/${name}.monit":
