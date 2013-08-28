2bed1975a755f8cc53adf8658bd74bfd9bf9bdf7
Jarl Stefansson <jarl@sdelements.com>
2013-08-28 16:52:56 -0400

    Fixed iptables script load order

--- modules/iptables/manifests/default_firewall.pp
+++ modules/iptables/manifests/default_firewall.pp
@@ -3,7 +3,7 @@ class iptables::default_firewall {
   case $operatingsystem {
     CentOS, RedHat: { $fw_rules_file = "/etc/sysconfig/iptables"}
     Ubuntu, Debian: { $fw_rules_file = "/etc/iptables.rules"}
- }
+  }
 
   file { "${fw_rules_file}":
     ensure => present,
@@ -15,25 +15,23 @@ class iptables::default_firewall {
 
 
   case $operatingsystem {
-    Ubuntu, Debian: { 
-      exec {"restart_ubuntu_networking":
-         command => "/usr/bin/nohup /bin/sh -c '/usr/sbin/invoke-rc.d networking stop; sleep 2; /usr/sbin/invoke-rc.d networking start'",
-      }
+    Ubuntu, Debian: {
       file { "/etc/network/if-pre-up.d/iptablesload":
         ensure => present,
         source => "puppet:///modules/iptables/iptablesload",
         owner => "root",
         group => "root",
         mode => "0700",
-        require => Exec["restart_ubuntu_networking"],
-      }
+      } ->
+      exec {"restart_ubuntu_networking":
+         command => "/usr/bin/nohup /bin/sh -c '/usr/sbin/invoke-rc.d networking stop; sleep 2; /usr/sbin/invoke-rc.d networking start'",
+      } ->
       file { "/etc/network/if-post-down.d/iptablessave":
         ensure => present,
         source => "puppet:///modules/iptables/iptablessave",
         owner => "root",
         group => "root",
         mode => "0700",
-        require => Exec["restart_ubuntu_networking"],
       }
     }
     CentOS, RedHat: {
