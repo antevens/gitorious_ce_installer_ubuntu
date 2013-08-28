6731857785dc6ad2280c987a47f8b7cb77e643eb
Jarl Stefansson <jarl@sdelements.com>
2013-08-28 14:51:22 -0400

    Fixed monit conf.d directory structure for debian

--- modules/gitorious/manifests/dependencies.pp
+++ modules/gitorious/manifests/dependencies.pp
@@ -4,7 +4,9 @@ class gitorious::dependencies {
   Exec { path => ["/opt/ruby-enterprise/bin","/usr/local/bin","/usr/bin","/bin", "/usr/sbin"] }
 
   case $operatingsystem {
-    CentOS: {       case $operatingsystemrelease {
+    CentOS: {
+      $monit_conf_dir = "/etc/monit.d"
+      case $operatingsystemrelease {
         /^5/: {
           $package_list = ["monit", "memcached", "ImageMagick","gcc-c++","zlib-devel","make","wget","libxml2","libxml2-devel","libxslt","libxslt-devel","gcc","ruby-devel","openssl","curl-devel"]
         }
@@ -14,6 +16,7 @@ class gitorious::dependencies {
       }
     }
     Ubuntu: { $package_list = ["monit", "memcached", "imagemagick","gobjc++","libghc-zlib-dev","make","wget","libxml2","libxml2-dev","libxslt1.1","libxslt1-dev","gcc","ruby-dev","openssl","libcurl4-openssl-dev","sendmail"]}
+    $monit_conf_dir = "/etc/monit/conf.d"
   }
 
   package { $package_list: ensure => installed }
@@ -38,7 +41,7 @@ class gitorious::dependencies {
     owner => "root",
     group => "root",
     mode => "0600",
-    source => "puppet:///modules/gitorious/config/monit.conf",
+    content => template("gitorious/etc/monit.conf.erb"),
     require => Package["monit"],
   }
 }
