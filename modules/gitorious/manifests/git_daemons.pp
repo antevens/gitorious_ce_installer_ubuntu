93a79ac1e3da62a622a2f767a37f4a56a3f0029c
Jarl Stefansson <jarl@sdelements.com>
2013-08-29 13:38:34 -0400

    Fixed monit daemons

--- modules/gitorious/manifests/git_daemons.pp
+++ modules/gitorious/manifests/git_daemons.pp
@@ -1,17 +1,19 @@
 class gitorious::ruby_git_daemons {
+  include monit
   monit::config { "git-daemon":
     t_app_root => $gitorious::app_root,
     t_control_scripts_dir => $gitorious::control_scripts_dir,
   }
-  file {"${gitorious::monit::monit_confd_dir}/git-daemons.monit":
+  file {"${monit::monit_confd_dir}/git-daemons.monit":
     ensure => absent,
   }
-  file {"${gitorious::monit::monit_confd_dir}/git-proxy.monit":
+  file {"${monit::monit_confd_dir}/git-proxy.monit":
     ensure => absent,
   }
 }
 
 class gitorious::native_git_daemons {
+  include monit
   case $operatingsystem {
     "CentOS", "RedHat": {
         $package_list = ["git-daemon"]
@@ -29,7 +31,7 @@ class gitorious::native_git_daemons {
 
   package { $package_list: ensure => installed }
 
-  file { "${gitorious::monit::monit_confd_dir}/git-daemon.monit":
+  file { "${monit::monit_confd_dir}/git-daemon.monit":
     ensure => absent,
   }
 }
